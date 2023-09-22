09101629f4ff06753230b00ebaf2bafccc3bd6cc
Sergey Nikolaev <73792465+dedMikola@users.noreply.github.com>
2023-09-22 13:54:10 +0300

    add click functions

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ function playAudio(sound_id) {
 }
 
 const buttons = {
-    KeyQ:'letter_q',
-    KeyW:'letter_w',
-    KeyE:'letter_e',
-    KeyA:'letter_a',
-    KeyS:'letter_s',
-    KeyD:'letter_d'
+    KeyQ: 'letter_q',
+    KeyW: 'letter_w',
+    KeyE: 'letter_e',
+    KeyA: 'letter_a',
+    KeyS: 'letter_s',
+    KeyD: 'letter_d'
 }
 
 document.addEventListener('keydown', (event) => {
-    if(event.code){
+    if (event.code) {
         playAudio(event.code);
         const el = document.querySelector(`.${buttons[event.code]}`);
 
@@ -20,11 +20,47 @@ document.addEventListener('keydown', (event) => {
         el.classList.add('press')
 
         //remove active class
-        setTimeout(()=>{
+        setTimeout(() => {
             el.classList.remove('press')
-        },300)
+        }, 300)
     }
 })
 
 
+// click on Q
+const elementQ = document.querySelector('.letter_q')
+elementQ.addEventListener('click', ()=>{
+    playAudio('KeyQ')
+})
 
+// click on W
+const elementW = document.querySelector('.letter_w')
+elementW.addEventListener('click', ()=>{
+    playAudio('KeyW')
+})
+
+//click on E
+const elementE = document.querySelector('.letter_e')
+elementE.addEventListener('click', ()=>{
+    playAudio('KeyE')
+})
+
+
+//click on A
+const elementA = document.querySelector('.letter_a')
+elementA.addEventListener('click', ()=>{
+    playAudio('KeyA')
+})
+
+
+//click on A
+const elementS = document.querySelector('.letter_s')
+elementS.addEventListener('click', ()=>{
+    playAudio('KeyS')
+})
+
+//click on A
+const elementD = document.querySelector('.letter_d')
+elementD.addEventListener('click', ()=>{
+    playAudio('KeyD')
+})
