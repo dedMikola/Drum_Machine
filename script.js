2092bc69bd58d197bcb6d482f5a166c7af1e5896
Sergey Nikolaev <73792465+dedMikola@users.noreply.github.com>
2023-09-25 14:04:39 +0300

    add data-key

--- script.js
+++ script.js
@@ -30,50 +30,10 @@ document.addEventListener('keydown', (event) => {
     }
 })
 
-// click on Q
-const elementQ = document.querySelector('.letter_q')
-elementQ.addEventListener('click', ()=>{
-    playAudio('KeyQ')
+const panel = document.querySelector('.panel')
+panel.addEventListener('click', (event) => {
+    const key = event.target.dataset['key'];
+    playAudio(`Key${key.toUpperCase()}`)
     //add and remove background for active element
-    addBackground(elementQ)
-})
-
-// click on W
-const elementW = document.querySelector('.letter_w')
-elementW.addEventListener('click', ()=>{
-    playAudio('KeyW')
-    //add and remove background for active element
-    addBackground(elementW)
-})
-
-//click on E
-const elementE = document.querySelector('.letter_e')
-elementE.addEventListener('click', ()=>{
-    playAudio('KeyE')
-    //add and remove background for active element
-    addBackground(elementE)
-})
-
-//click on A
-const elementA = document.querySelector('.letter_a')
-elementA.addEventListener('click', ()=>{
-    playAudio('KeyA')
-    //add and remove background for active element
-    addBackground(elementA)
-})
-
-//click on S
-const elementS = document.querySelector('.letter_s')
-elementS.addEventListener('click', ()=>{
-    playAudio('KeyS')
-    //add and remove background for active element
-    addBackground(elementS)
-})
-
-//click on D
-const elementD = document.querySelector('.letter_d')
-elementD.addEventListener('click', ()=>{
-    playAudio('KeyD')
-    //add and remove background for active element
-    addBackground(elementD)
+    addBackground(event.target)
 })
